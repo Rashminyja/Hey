2706f2211791a782762f1fc1f442018371fb3a37
Rashminyja <48384246+Rashminyja@users.noreply.github.com>
2019-03-29 15:11:09 +0530

    Add files via upload

---
+++ 223.py
@@ -0,0 +1,3 @@
+print("hii i am  *****")
+print("i am studing in cmru")
+print("i live in blore")
---
+++ graph.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+#list of vales x axis
+x=[2,4,6,8,10]
+y=[1,3,5,7,9]
+plt.plot(x,y)
+plt.show()
